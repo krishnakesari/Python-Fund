2d84d5014f537041127d9e4b2d93222e48458647
krishna <bkesari@outlook.com>
2020-06-01 18:01:29 -0400

    commit

--- Basics.py
+++ Basics.py
@@ -11,7 +11,7 @@ print('This is python version {}'.format(platform.python_version()))
 ## Hello Main - Define functions before they are called 
 
 def main():
-    message()
+    message()  # Indented function adds white space 
 
 def message():
     print('This is python version {}'.format(platform.python_version()))
@@ -21,4 +21,9 @@ if __name__ == '__main__': main()
 
 ## Expressions - literals, identifiers, values, function calls and operations
 ## Statement is a line of code
+
+## White spaces - Indented function
+
+x = 42
+print('x = {}'.format(x))
 
