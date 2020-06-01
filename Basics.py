303c64790e067b3c1164391f5709355180468472
krishna <bkesari@outlook.com>
2020-06-01 17:55:22 -0400

    commit

--- Basics.py
+++ Basics.py
@@ -1,3 +1,24 @@
 print('Hello, World.')
 
 print('Something entirely different')
+
+## Expressions and statements
+
+import platform
+print('This is python version {}'.format(platform.python_version()))
+
+
+## Hello Main - Define functions before they are called 
+
+def main():
+    message()
+
+def message():
+    print('This is python version {}'.format(platform.python_version()))
+
+if __name__ == '__main__': main()
+
+
+## Expressions - literals, identifiers, values, function calls and operations
+## Statement is a line of code
+
