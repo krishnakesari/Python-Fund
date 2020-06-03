149526f4ac8c5d5bae74736ec3a3cbb4f723435f
krishna <bkesari@outlook.com>
2020-06-03 11:20:03 -0400

    commit

---
+++ Conditions.py
@@ -0,0 +1,20 @@
+if True:
+    print('if true')
+elif False:
+    print('elif true')
+else:
+    print('neither true')
+
+
+x = 5
+if x == 0:
+    print("one True")
+elif x == 5:
+    print("Five True")
+
+
+# Conditional Operators
+hungry = True
+x = 'Feed bear' if hungry else 'Do not feed'
+print(x)
+
