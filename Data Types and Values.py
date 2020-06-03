0a0bdbfcd70732cbd9a8c7f89a5fa399c647679a
krishna <bkesari@outlook.com>
2020-06-03 07:12:04 -0400

    commit

--- Data Types and Values.py
+++ Data Types and Values.py
@@ -4,6 +4,32 @@ print('x is {}'.format(x))
 print(type(x))
 
 #  String Type
-x = 'Seven'
+
+x = 'seven'.upper()
 print('x is {}'.format(x))
 print(type(x))
+
+## F -strings
+a = 8
+b = 9
+
+x = f'seven {a} {b}'
+print('x is {}'.format(x))
+print(type(x))
+
+### OR
+
+x = 'seven {} {}'.format(8,9)
+print('x is {}'.format(x))
+print(type(x))
+
+# Numeric Type
+x = 7
+print('x is {}'.format(x))
+print(type(x))
+
+y = 7 % 3 # Division 
+print(y)
+
+z = 7 // 3 # Reminder
+print(z)
